5c2a716192adbbde9567b12065762bf871bd0360
alukos <alukos@yandex.ru>
2018-01-30 12:28:38 +0300

    try proxy

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div class="container">
+    <div>{{ list }}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ req, params }) {
+    let { data } = await axios.get('/depth/ltc_btc-nmc_btc')
+    return { list: data};
+  },
+  head: {
+    title: 'List'
+  }
+}
+</script>
